5424a1664e7cc675480f8d6ab215fbbf30448634
predo505 <pedroesquecido1@gmail.com>
2025-03-28 14:05:01 -0300

    Atualizar o Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
 
+        //Variáveis
         String senha;
         String senhaCorreta = "Java21";
 
+        //Confere a senha
         do {
             System.out.println("Insira sua senha: ");
             senha = scanner.nextLine();
